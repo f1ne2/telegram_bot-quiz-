cc89e78f07457c6d7cf8bb5dee9a8370a6c4ef8f
f1ne2 <alextomilovski@gmail.com>
2021-06-12 22:11:43 +0300

    structure

---
+++ bot/db.py
@@ -0,0 +1,22 @@
+from bot.interface import FormalInterface
+import redis
+
+
+class WorkWithDB(FormalInterface):
+    def __init__(self):
+        self.redisClient = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+    def push(self, chat_id: str, value: str):
+        return self.redisClient.lpush(f'{chat_id}', value)
+
+    def delete(self, chat_id: str):
+        return self.redisClient.delete(f'{chat_id}')
+
+    def exist(self, chat_id: str):
+        return self.redisClient.exists(f'{chat_id}')
+
+    def len(self, chat_id: str):
+        return self.redisClient.llen(f'{chat_id}')
+
+    def range(self, chat_id: str):
+        return self.redisClient.lrange(f'{chat_id}', 0, self.redisClient.llen(f'{chat_id}')-2)
--- main.py
+++ main.py
@@ -3,40 +3,41 @@ from flask import Flask, request
 from dotenv import load_dotenv
 import os
 from os.path import join, dirname
+from bot.db import WorkWithDB
+from bot.interface import FormalInterface
 import requests
 import json
-import redis
 
 app = Flask(__name__)
-redisClient = redis.Redis(host='127.0.0.1', port=6379, db=0)
+db: FormalInterface = WorkWithDB()
 
 
-def send_message(chat_id, text) -> None:
-    method = "sendMessage"
-    token = get_from_env("TELEGRAM_BOT_TOKEN")
+def send_message(chat_id: str, text: str) -> None:
+    method: str = "sendMessage"
+    token: str = get_from_env("TELEGRAM_BOT_TOKEN")
     url = f"https://api.telegram.org/bot{token}/{method}"
     data = {"chat_id": chat_id, "text": text}
     requests.post(url, data=data)
 
 
-def get_from_env(key):
+def get_from_env(key: str) -> str:
     dotenv_path = join(dirname(__file__), ".env")
     load_dotenv(dotenv_path)
     return os.environ.get(key)
 
 
-def get_answers():
-    with open('questions.json', 'r', encoding='utf-8') as f:
-        question = json.load(f)
+def get_answers() -> json:
+    with open('assets/questions.json', 'r', encoding='utf-8') as f:
+        question: json = json.load(f)
     return question
 
 
-def get_result(chat_id):
-    correct_answers = 0
-    answer_arr = redisClient.lrange(f'{chat_id}', 0, redisClient.llen(f'{chat_id}')-2)
+def get_result(chat_id: str) -> int:
+    correct_answers: int = 0
+    answer_arr: [] = db.range(f'{chat_id}')
     answer_arr = [line.decode("utf-8").rstrip().lower() for line in answer_arr]
     answer_arr = answer_arr[::-1]
-    question = get_answers()
+    question: json = get_answers()
     for i in range(len(answer_arr)):
         if answer_arr[i] == question["questions"][i]["answer"]:
             correct_answers += 1
@@ -44,28 +45,28 @@ def get_result(chat_id):
 
 
 @app.route("/", methods=["GET", "POST"])
-def receive_update():
+def receive_update() -> json:
     if request.method == "POST":
         msg = request.get_json()
         chat_id = msg["message"]["chat"]["id"]
         question = get_answers()
 
         if request.json["message"]["text"] == "/start":
-            if redisClient.exists(f'{chat_id}'):
-                redisClient.delete(f'{chat_id}')
+            if db.exist(f'{chat_id}'):
+                db.delete(f'{chat_id}')
             send_message(chat_id, "Welcome to the quiz!!! \n You have to answer the 5 questions for 5 minutes \n "
                                   "Let's go!  ")
-            redisClient.lpush(f'{chat_id}', 'answers')
+            db.push(f'{chat_id}', 'answers')
             send_message(chat_id, question["questions"][0]["question"])
-        elif redisClient.exists(f'{chat_id}'):
-            redisClient.lpush(f'{chat_id}', request.json["message"]["text"])
-            if redisClient.llen(f'{chat_id}')-1 == len(question["questions"]):
-                result = get_result(chat_id)
-                send_message(chat_id, f"{result}/{redisClient.llen(f'{chat_id}')-1} "
+        elif db.exist(f'{chat_id}'):
+            db.push(f'{chat_id}', request.json["message"]["text"])
+            if db.len(f'{chat_id}')-1 == len(question["questions"]):
+                result: int = get_result(chat_id)
+                send_message(chat_id, f"{result}/{db.len(f'{chat_id}')-1} "
                                       f"correct answers. \n Enter /start if you would try again")
-                redisClient.delete(f'{chat_id}')
+                db.delete(f'{chat_id}')
             else:
-                send_message(chat_id, question["questions"][redisClient.llen(f'{chat_id}')-1]["question"])
+                send_message(chat_id, question["questions"][db.len(f'{chat_id}')-1]["question"])
 
     return {"ok": True}
 
