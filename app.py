7e459baae9ae34e15e2e2e2e6212c32cc1d3b0fa
f1ne2 <alextomilovski@gmail.com>
2021-06-12 18:03:26 +0300

    bot with redis

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import os
 from os.path import join, dirname
 import requests
 import json
+import redis
 
 app = Flask(__name__)
+redisClient = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
 
 def send_message(chat_id, text) -> None:
@@ -23,16 +25,18 @@ def get_from_env(key):
     return os.environ.get(key)
 
 
-def get_result(chat_id):
-    answer_arr = []
-    correct_answers = 0
-    with open(f'{chat_id}.txt', 'r', encoding='utf-8') as f:
-        for line in f:
-            answer_arr.append(line)
-    answer_arr = [line.rstrip() for line in answer_arr]
-    answer_arr = [line.lower() for line in answer_arr]
+def get_answers():
     with open('questions.json', 'r', encoding='utf-8') as f:
         question = json.load(f)
+    return question
+
+
+def get_result(chat_id):
+    correct_answers = 0
+    answer_arr = redisClient.lrange(f'{chat_id}', 0, redisClient.llen(f'{chat_id}')-2)
+    answer_arr = [line.decode("utf-8").rstrip().lower() for line in answer_arr]
+    answer_arr = answer_arr[::-1]
+    question = get_answers()
     for i in range(len(answer_arr)):
         if answer_arr[i] == question["questions"][i]["answer"]:
             correct_answers += 1
@@ -44,31 +48,25 @@ def receive_update():
     if request.method == "POST":
         msg = request.get_json()
         chat_id = msg["message"]["chat"]["id"]
+        question = get_answers()
+
         if request.json["message"]["text"] == "/start":
-            if os.path.exists(f'{chat_id}.txt'):
-                os.remove(f'{chat_id}.txt')
-            send_message(chat_id, "Welcome to the quiz!!! Enter /quiz to quiz start")
-        elif request.json["message"]["text"] == "/quiz":
-            with open('questions.json', 'r', encoding='utf-8') as f:
-                question = json.load(f)
-            with open(f'{chat_id}.txt', 'w', encoding='utf-8') as f:
-                pass
+            if redisClient.exists(f'{chat_id}'):
+                redisClient.delete(f'{chat_id}')
+            send_message(chat_id, "Welcome to the quiz!!! \n You have to answer the 5 questions for 5 minutes \n "
+                                  "Let's go!  ")
+            redisClient.lpush(f'{chat_id}', 'answers')
             send_message(chat_id, question["questions"][0]["question"])
-        else:
-            if os.path.exists(f'{chat_id}.txt'):
-                with open(f'{chat_id}.txt', 'a', encoding='utf-8') as file:
-                    file.write(msg["message"]["text"] + '\n')
-                answer_arr = []
-                with open(f'{chat_id}.txt', 'r', encoding='utf-8') as f:
-                    for line in f:
-                        answer_arr.append(line)
-                with open('questions.json', 'r', encoding='utf-8') as f:
-                    question = json.load(f)
-                if len(answer_arr) == len(question["questions"]):
-                    result = get_result(chat_id)
-                    send_message(chat_id, f"{result}/5 correct answers. \n Enter /start if you would try again")
-                else:
-                    send_message(chat_id, question["questions"][len(answer_arr)]["question"])
+        elif redisClient.exists(f'{chat_id}'):
+            redisClient.lpush(f'{chat_id}', request.json["message"]["text"])
+            if redisClient.llen(f'{chat_id}')-1 == len(question["questions"]):
+                result = get_result(chat_id)
+                send_message(chat_id, f"{result}/{redisClient.llen(f'{chat_id}')-1} "
+                                      f"correct answers. \n Enter /start if you would try again")
+                redisClient.delete(f'{chat_id}')
+            else:
+                send_message(chat_id, question["questions"][redisClient.llen(f'{chat_id}')-1]["question"])
+
     return {"ok": True}
 
 
